e26292412468831b766281f21e09880169e4ab89
GJ Tiquia <gershom.tiquia@gmail.com>
2025-05-11 14:17:44 -0400

    adjust ingredient spawn algorithm for better gameplay (each ingredient gets spawned in a random cycle)

--- Assets/Scripts/IngredientSpawner.cs
+++ Assets/Scripts/IngredientSpawner.cs
@@ -11,6 +11,8 @@ public class IngredientSpawner : MonoBehaviour
     [Header("References")]
     [SerializeField] private BoxCollider _boxCollider;
 
+    private List<int> _previouslySpawnedIndexes = new(); // ensures each prefab gets spawned once before resetting the "pool" again
+
     private void Awake()
     {
         Instance = this;
@@ -25,9 +27,16 @@ public class IngredientSpawner : MonoBehaviour
         var randZ = Random.Range(bounds.min.z, bounds.max.z);
         var randPos = new Vector3(randX, randY, randZ);
 
-        var randomIndex = Random.Range(0, _ingredientPrefabs.Count);
-        var randomPrefab = _ingredientPrefabs[randomIndex];
+        if (_previouslySpawnedIndexes.Count >= _ingredientPrefabs.Count)
+            _previouslySpawnedIndexes.Clear();
 
+        var randomIndex = Random.Range(0, _ingredientPrefabs.Count);
+        while (_previouslySpawnedIndexes.Contains(randomIndex))
+            randomIndex = Random.Range(0, _ingredientPrefabs.Count);
+
+        _previouslySpawnedIndexes.Add(randomIndex);
+
+        var randomPrefab = _ingredientPrefabs[randomIndex];
         var instance = Instantiate(randomPrefab, randPos, Quaternion.identity);
 
         var randomDirection = Random.onUnitSphere;
